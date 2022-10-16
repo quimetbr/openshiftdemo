ea73d6fefe9768483367f263853f0876340fa3ca
quimetbr <joaquim.castells@gmail.com>
2022-10-16 08:13:15 -0300

    Add files via upload
    hello

---
+++ src/main/java/com/quim/hello/HelloOpenShiftApplication.java
@@ -0,0 +1,13 @@
+package com.quim.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloOpenShiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloOpenShiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quim/hello/HelloOpenShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quim.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloOpenShiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
